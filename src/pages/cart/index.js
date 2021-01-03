d778e29c2d7be375576cdf53d40e5c32f3744edb
angelhpascual <angelhpascual@pop-os.localdomain>
2021-01-03 18:43:32 +0100

    handleckecout bug FIXED

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -122,7 +122,7 @@ const Cart = () => {
                      :
                      
                       cityPopupDone()
-                      handleCheckout();
+                      
                     
                   }}>
                   CHECKOUT
