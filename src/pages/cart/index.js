ed1ba6686e7a077870d50ea06051535b69ab2de4
angelhpascual <angelhpascual@pop-os.localdomain>
2021-01-03 18:41:21 +0100

    final deploy

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -122,6 +122,7 @@ const Cart = () => {
                      :
                      
                       cityPopupDone()
+                      handleCheckout();
                     
                   }}>
                   CHECKOUT
