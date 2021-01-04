5022bb00e63608638a97c30793f7974320d5e9b7
angelhpascual <angelhpascual@pop-os.localdomain>
2021-01-04 03:38:00 +0100

    About link FIXED

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import Layout from '../../components/Layout';
-import { GitHubIcon} from '../../components/icons'
+import React from "react";
+import Layout from "../../components/Layout";
+import { GitHubIcon } from "../../components/icons";
 
 const About = () => {
-    
-    return ( 
-        <Layout title="About" description="This is the About page" >
-            <div className="text-center mt-5 p-3">
-                <h1>About</h1>
-                <p className="mt-3">This project was built for test purposes using <strong>Context API</strong> by angelhpascual for Decorabaño.com.</p>
+  return (
+    <Layout title="About" description="This is the About page">
+      <div className="text-center mt-5 p-3">
+        <h1>About</h1>
+        <p className="mt-3">
+          This project was built for test purposes using{" "}
+          <strong>Context API</strong> by angelhpascual for Decorabaño.com.
+        </p>
 
-                <a className="btn btn-primary mt-4" href="https://github.com/Angelhpascual/cart-decorabano-deploy">
-                    <GitHubIcon width={"18px"}/> <span className="ml-2 mr-4">Visit Repo</span></a>
-            </div>
-        </Layout>
-     );
-}
- 
+        <a
+          className="btn btn-primary mt-4"
+          href="https://github.com/Angelhpascual/cart-decorabano-final-deploy"
+        >
+          <GitHubIcon width={"18px"} />{" "}
+          <span className="ml-2 mr-4">Visit Repo</span>
+        </a>
+      </div>
+    </Layout>
+  );
+};
+
 export default About;
